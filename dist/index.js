129c6e67caca2458d51017ab7ad3b3c853532c16
Devlon Duthie <duthied@gmail.com>
2021-04-28 22:09:40 -0400

    Another build

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-/******/ (() => { // webpackBootstrap
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 941:
@@ -77,3 +77,4 @@ try {
 module.exports = __webpack_exports__;
 /******/ })()
 ;
+//# sourceMappingURL=index.js.map
