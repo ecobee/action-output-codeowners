7f1d790704c7489ffb32617f21e399f953715ecc
Devlon Duthie <duthied@gmail.com>
2021-04-30 09:21:53 -0400

    Tidy up

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const github = require('@actions/github');
 try {
   // `author` input defined in action metadata file
   const author = core.getInput('author');
-  console.log(`Hello ${author}!`);
+  // console.log(`Hello ${author}!`);
+  
   // forcing to true for now
   core.setOutput("is_contributor", author);
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
