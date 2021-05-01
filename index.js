ffc3c04a9771fcd3cfe6139a178ae32d758df216
Devlon Duthie <duthied@gmail.com>
2021-04-30 23:05:54 -0400

    ...

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 const core = require('@actions/core');
 const codeOwnersUtils = require('codeowners-utils');
 
-async function getOwners(path) {
-  // get the code owners
-  const codeownerPath = path || './CODEOWNERS'
-  const results = await codeOwnersUtils.loadOwners(codeownerPath);
-
-  // remove the `@` to compare to author
-  const cleanedOwners = results.map(line => {
-    return line.name.substring(1);
-  })
-}
-
 (async () => {
   try {
     // `author` input defined in action metadata file
     const author = core.getInput('author');
   
-    owners = getOwners(core.getInput('path'));
-    // is author in the array?
-    const is_contributor = owners.some(line => line.name === author)
+    // get the code owners
+    const codeownerPath = core.getInput('path') || './CODEOWNERS'
+    const results = await codeOwnersUtils.loadOwners(codeownerPath);
 
+    // remove the `@` to compare to author
+    const cleanedOwners = results.map(line => {
+      let owner = line.owners[0];
+      if (owner.charAt(0)) {
+        owner = owner.slice(1);
+      }
+      return owner;
+    });
+
+    // is author in the array?
+    const is_contributor = cleanedOwners.includes(author)
     core.setOutput("is_contributor", is_contributor);
   
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
-    
   } catch (error) {
     core.setFailed(error.message);
   }
