9f778e4688ad0e11a82dd7cd474dc3bbadf59192
Devlon Duthie <duthied@gmail.com>
2021-05-22 19:30:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ const util = require('./util');
   try {
 
     // get the code owners
-    const codeownerPath = core.getInput('owners_location') || './CODEOWNERS'
-    core.debug(` codeownerPath -> ${codeownerPath}`);
+    let codeownerPath;
+    if !(core.getInput('owners_location')) {
+      codeownerPath = './CODEOWNERS'
+      core.debug(` codeownerPath not provided, defaulting: ${codeownerPath}`);
+    } else {
+      codeownerPath = core.getInput('owners_location')
+      core.debug(` codeownerPath provided: ${codeownerPath}`);
+    }
+    
+//     const codeownerPath = core.getInput('owners_location') || './CODEOWNERS'
+//     core.debug(` codeownerPath -> ${codeownerPath}`);
 
     const owners = await util.loadOwners(codeownerPath);
 
     // remove the `@` to compare to author
     const cleanedOwners = owners.map(owner => {
-       core.debug(`owner:${owner}`);
+       core.debug(`owner: ${owner}`);
       if (owner.charAt(0)) {
         owner = owner.slice(1);
       }
       return owner;
     });
 
-    core.debug(`cleanedOwners:${cleanedOwners}`);
+    core.debug(`cleanedOwners: ${cleanedOwners}`);
     core.setOutput("owners", cleanedOwners);
   
   } catch (error) {
