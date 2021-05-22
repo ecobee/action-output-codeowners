58a76bd984940e0bb3a9549e4ff0f5f14f569f33
Devlon Duthie <duthied@gmail.com>
2021-05-22 19:32:11 -0400

    Update util.js

--- util.js
+++ util.js
@@ -5,16 +5,24 @@ const core = require('@actions/core');
 let readFile = util.promisify(fs.readFile)
 
 async function loadOwners(path) {
-	if (!path) return null
-	let contents = await readFile(path, "utf-8")
-	let entries = parse(contents)
-	return entries
+  core.debug(` [loadOwners] -> path: ${path}`);
+  if (!path) return null
+  
+  let contents = await readFile(path, "utf-8")
+  core.debug(` [loadOwners] -> contents: ${contents}`);
+
+  let entries = parse(contents)
+  core.debug(` [loadOwners] -> entries: ${entries}`);
+
+  return entries
 }
 
 function parse(contents) {
   const regex = /\B\@([a-zA-Z'-_]+)/gm;
   const entries = contents.match(regex);
-	return entries;
+	core.debug(` [parse] -> entries: ${entries}`);
+
+  return entries;
 }
 
 module.exports = {
