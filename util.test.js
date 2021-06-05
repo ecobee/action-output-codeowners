c9d1aa4fba53b37c165fe5fe32702271284b92bb
Devlon Duthie <duthied@gmail.com>
2021-06-05 17:19:05 -0400

    Check point before refactoring how fs is being used

--- util.test.js
+++ util.test.js
@@ -1,8 +1,12 @@
-const { getCodeOwnersPath } = require('./util');
+const { getCodeOwnersPath, cleanCodeOwners, loadOwners } = require('./util');
 
-describe("Get codeowners path",() => {
-  let defaultPath = './CODEOWNERS';
-  let pathProvided = '.some_other_path/CODEOWNERS';
+const mockCodeownersPath = './some_path';
+const mockCodeownersName = 'CODEOWNERS';
+// const mockCodeownersContents = '/ @one @two';
+
+describe("Get codeowners path", () => {
+  const defaultPath = './CODEOWNERS';
+  const pathProvided = `${mockCodeownersPath}/${mockCodeownersName}`;
 
   test("if path is not provided, should return default", () => {
     expect(getCodeOwnersPath(null)).toBe(defaultPath);
@@ -11,3 +15,36 @@ describe("Get codeowners path",() => {
     expect(getCodeOwnersPath(pathProvided)).toBe(pathProvided);
   });
 });
+
+describe("Clean codeowners contents", () => {
+  const input = ['@one', '@two'];
+  const expecting = ['one', 'two'];
+
+  test("array length should remain the same and @ symbol should be removed", () => {
+    expect(cleanCodeOwners(input)).toEqual(expect.arrayContaining(expecting));
+  });
+});
+
+// describe("Load owners", () => {
+
+//   beforeAll(() => {});
+
+//   afterAll(() => {});
+
+//   test("should return null if passed null", async () => {
+//     expect.assertions(1);
+//     const response = await loadOwners(null);
+
+//     expect(response).toBe(null);
+//   });  
+
+//   test("should parse the entries, returning an array of the contents", async () => {
+//     const pathProvided = `${mockCodeownersPath}/${mockCodeownersName}`;
+//     const expected = ['@one', '@two']
+
+//     expect.assertions(1);
+//     const response = await loadOwners(pathProvided);
+
+//     expect(response).toBe(expected);
+//   });
+// });
